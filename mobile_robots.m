da90ef1f00738e3a8544f231501d2c0157885847
KPiekorz <piekorz@student.agh.edu.pl>
2021-12-11 19:54:22 +0100

    refactor

--- mobile_robots.m
+++ mobile_robots.m
@@ -1,5 +1,6 @@
 clc;
 clear all;
+close all;
 
 L = 1; 
 kp1 = 0.5;
@@ -12,12 +13,14 @@ y_set = 5;
 
 x = [8 7 5 3 2 3 5 7];
 y = [5 7 8 7 5 3 2 3];
-theta = [pi/2 5*pi/2 pi 9*pi/8 3*pi/2 13*pi/8 0 pi/4];
+theta = [pi/2 5*pi/8 pi 9*pi/8 3*pi/2 13*pi/8 0 pi/4];
 
 figure(1);
 grid on;
 title("Placement of vechicle to selected point [5,5]");
-
+xlabel("x[m]");
+ylabel("y[m]");
+axis([0 10 0 10])
 
 for i = 1:8
     x_start = x(i);
@@ -26,5 +29,5 @@ for i = 1:8
     out = sim("mobile_robot_control.slx");
     
     hold on;
-    plot(out.x, out.y);
+    plot(out.x, out.y, 'g');
 end
