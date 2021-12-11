5b22541d9c7b294e0509a9282f5cc18cdd870941
KPiekorz <piekorz@student.agh.edu.pl>
2021-12-11 19:26:56 +0100

    matlab simulation

--- mobile_robots.m
+++ mobile_robots.m
@@ -5,5 +5,20 @@ L = 1;
 kp1 = 0.5;
 kp2 = 4;
 
-x_set =  5;
+%% Control of two wheel robot
+
+x_set = 5;
 y_set = 5;
+
+x_start = 5;
+y_start = 8;
+theta_start = pi;
+
+out = sim("mobile_robot_control.slx");
+
+figure(1);
+plot(out.x, out.y);
+
+for r = 1:8
+    
+end
